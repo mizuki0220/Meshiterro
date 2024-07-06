5ffa8e4df23baa03e71b87f3f5a4c255f6c82450
EC2 Default User <ec2-user@ip-172-31-0-230.ap-northeast-1.compute.internal>
2024-07-06 16:47:24 +0000

    [finish]15章

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @post_images = @user.post_images
+  end
+
+  def edit
+  end
+  
+  private
+  def user_params
+    params.permit(:name, :email, :password)
+  end
+
+end
