5d1fddda7f8cc19f07b651c3854de6eaedee53bf
mizuki0220 <em9220me.i@gmail.com>
2024-08-05 15:35:39 +0000

    [finish]構成の改善

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -1,5 +1,5 @@
 class Admin::DashboardsController < ApplicationController
-  before_action :authenticate_admin!
+  layout 'admin'
     def index
         @users = User.all
     end 
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,8 +1,17 @@
 # frozen_string_literal: true
 
-class Admin::SessionsController < Devise::SessionsController
-  layout 'admin'
+class Public::SessionsController < Devise::SessionsController
+  before_action :configure_permitted_parameters, if: :devise_controller?
   # before_action :configure_sign_in_params, only: [:create]
+  
+  def after_sign_in_path_for(resource)
+    post_images_path
+  end
+
+  def after_sign_out_path_for(resource)
+    about_path
+  end
+
 
   # GET /resource/sign_in
   # def new
@@ -25,14 +34,11 @@ class Admin::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  
   protected
-
-  def after_sign_in_path_for(resource)
-    admin_dashboards_path
-  end
-
-  def after_sign_out_path_for(resource_or_scope)
-    new_admin_session_path
+ 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
 end
